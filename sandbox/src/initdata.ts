d12d5d6610cddfc2f737edafc7e8e7ac9e5f7741
Rémi Van Keisbelck <remi@rvkb.com>
2024-04-15 12:28:56 +0200

    fix string custom renderer sample

---
+++ sandbox/src/initdata.ts
@@ -0,0 +1,333 @@
+import big_sample from "./big_sample.json";
+
+const Sample_Long = `{
+    "type": [
+      "integer",
+      "null"
+    ],
+    "format": "int64"
+  }`;
+
+const Sample_String = `{
+    "type": [
+      "string",
+      "null"
+    ]
+  }`;
+
+const Sample_EnumArray = `{
+    "type": [
+      "array", "null"
+    ],
+    "items": {
+      "type": [ "string", "null" ],
+      "enum": [
+        "FOO", "BAR"
+      ]
+    }
+  }`;
+
+const Sample_BeanContainingOtherBean = `{
+    "$schema": "https://json-schema.org/draft/2019-09/schema",
+    "$id": "http://schema.BeanWithBean",
+    "type": "object",
+    "properties": {
+      "customer": {
+        "$ref": "#/definitions/schema.Customer"
+      }
+    },
+    "definitions": {
+      "schema.Customer": {
+        "type": "object",
+        "properties": {
+          "firstName": {
+            "type": [
+              "string",
+              "null"
+            ]
+          },
+          "lastName": {
+            "type": [
+              "string",
+              "null"
+            ]
+          },
+          "amount": {
+            "type": "number",
+            "format": "double"
+          },
+          "age": {
+            "type": "integer",
+            "format": "int32"
+          }
+        }
+      }
+    }
+  }`;
+
+const Sample_Inheritance = `{
+    "$schema": "https://json-schema.org/draft/2019-09/schema",
+    "$id": "http://schema.shape.Rectangle",
+    "type": "object",
+    "allOf": [
+      {
+        "$ref": "#/definitions/schema.shape.Shape"
+      }
+    ],
+    "properties": {
+      "height": {
+        "type": "integer",
+        "format": "int32"
+      },
+      "width": {
+        "type": "integer",
+        "format": "int32"
+      }
+    },
+    "definitions": {
+      "schema.shape.Shape": {
+        "type": "object",
+        "properties": {
+          "name": {
+            "type": [
+              "string",
+              "null"
+            ]
+          }
+        }
+      }
+    }
+  }`;
+
+const Example_Polymorphism = `{
+    "$schema": "https://json-schema.org/draft/2019-09/schema",
+    "$id": "http://schema.animal.Animal",
+    "type": "object",
+    "allOf": [
+      {
+        "if": {
+          "properties": {
+            "what": {
+              "type": "string",
+              "const": "schema.animal.Lion"
+            }
+          }
+        },
+        "then": {
+          "$ref": "#/definitions/schema.animal.Lion"
+        }
+      },
+      {
+        "if": {
+          "properties": {
+            "what": {
+              "type": "string",
+              "const": "schema.animal.Elephant"
+            }
+          }
+        },
+        "then": {
+          "$ref": "#/definitions/schema.animal.Elephant"
+        }
+      }
+    ],
+    "definitions": {
+      "schema.animal.Animal": {
+        "properties": {
+          "name": {
+            "type": [
+              "string",
+              "null"
+            ]
+          },
+          "sound": {
+            "type": [
+              "string",
+              "null"
+            ]
+          },
+          "type": {
+            "type": [
+              "string",
+              "null"
+            ]
+          },
+          "endangered": {
+            "type": "boolean"
+          }
+        }
+      },
+      "schema.animal.Lion": {
+        "allOf": [
+          {
+            "$ref": "#/definitions/schema.animal.Animal"
+          }
+        ],
+        "properties": {
+          "mane": {
+            "type": "boolean"
+          }
+        }
+      },
+      "schema.animal.Elephant": {
+        "allOf": [
+          {
+            "$ref": "#/definitions/schema.animal.Animal"
+          }
+        ],
+        "properties": {
+          "trunkLength": {
+            "type": "number",
+            "format": "double"
+          },
+          "tusk": {
+            "type": "boolean"
+          }
+        }
+      }
+    }
+  }`;
+
+const Example_Cycle = `{
+    "$schema": "https://json-schema.org/draft/2019-09/schema",
+    "$id": "http://schema.TestCyclic$Loop",
+    "type": "object",
+    "properties": {
+      "next": {
+        "$ref": "http://schema.TestCyclic$Loop"
+      },
+      "name": {
+        "type": [
+          "string",
+          "null"
+        ]
+      }
+    }
+  }`;
+
+const Example_Unwrapping = `{
+    "$schema": "https://json-schema.org/draft/2019-09/schema",
+    "$id": "http://schema.TestUnwrapping$UnwrappingRoot",
+    "type": "object",
+    "properties": {
+      "age": {
+        "type": "integer",
+        "format": "int32"
+      },
+      "name.first": {
+        "type": [
+          "string",
+          "null"
+        ]
+      },
+      "name.last": {
+        "type": [
+          "string",
+          "null"
+        ]
+      }
+    }
+  }`;
+
+const Example_Renderer1 = `{
+      "type": "string",
+      "renderer": "MyStringRenderer"
+  }`;
+
+const Example_Renderer2 = `{
+      "type": "string",
+      "renderer": {
+          "key": "MyStringRenderer",
+          "myConfigProp": 123
+      }
+  }`;
+
+const Example_Renderer3 = JSON.stringify(
+  {
+    properties: {
+      name: {
+        type: "string",
+      },
+      rating: {
+        type: "number",
+        renderer: "RatingRenderer",
+      },
+    },
+  },
+  null,
+  "  "
+);
+
+const Example_Renderer4 = `
+  {
+    "renderer": "MyObjectRenderer",
+    "properties": {
+      "name": {
+        "type": "string"
+      },
+      "rating": {
+        "type": "number",
+        "renderer": "RatingRenderer"
+      }
+    }
+  }`;
+
+const Example_Date = JSON.stringify(
+  {
+    type: "string",
+    format: "date",
+  },
+  null,
+  "  "
+);
+
+const Example_Time = JSON.stringify(
+  {
+    type: "string",
+    format: "time",
+  },
+  null,
+  "  "
+);
+
+const Example_DateTime = JSON.stringify(
+  {
+    type: "string",
+    format: "date-time",
+  },
+  null,
+  "  "
+);
+
+const Example_DateTimeWithExample = `{
+    "type": [ "string", "null" ],
+    "format": "date-time",
+    "examples": [ "2022-11-28T09:27:17Z" ]
+  }`;
+
+export const samples = [
+  ["All", "{}"],
+  ["Long", Sample_Long],
+  ["String", Sample_String],
+  ["EnumArray", Sample_EnumArray],
+  ["BeanContainingOtherBean", Sample_BeanContainingOtherBean],
+  ["Inheritance", Sample_Inheritance],
+  ["Polymorphism", Example_Polymorphism],
+  ["Cycle", Example_Cycle],
+  ["Unwrapping", Example_Unwrapping],
+  ["Date", Example_Date],
+  ["Time", Example_Time],
+  ["DateTime", Example_DateTime],
+  ["DateTimeExample", Example_DateTimeWithExample],
+  ["BigSample", JSON.stringify(big_sample, null, "  ")],
+  ["Renderer1", Example_Renderer1],
+  ["Renderer2", Example_Renderer2],
+  ["RendererRating", Example_Renderer3],
+  ["RendererObject", Example_Renderer4],
+];
+
+export const initialSchema = {
+  type: "array",
+  items: { type: "string", renderer: "MyStringRenderer" },
+};
+export const initialValue = ["a", "b"];
--- sandbox/src/text-editor.ts
+++ sandbox/src/text-editor.ts
@@ -14,146 +14,160 @@
  * limitations under the License.
  */
 
-import 'monaco-editor/esm/vs/editor/editor.all.js';
+import "monaco-editor/esm/vs/editor/editor.all.js";
 
 // // support all editor features
-import 'monaco-editor/esm/vs/editor/standalone/browser/accessibilityHelp/accessibilityHelp.js';
-import 'monaco-editor/esm/vs/editor/standalone/browser/inspectTokens/inspectTokens.js';
-import 'monaco-editor/esm/vs/editor/standalone/browser/iPadShowKeyboard/iPadShowKeyboard.js';
-import 'monaco-editor/esm/vs/editor/standalone/browser/quickAccess/standaloneHelpQuickAccess.js';
-import 'monaco-editor/esm/vs/editor/standalone/browser/quickAccess/standaloneGotoLineQuickAccess.js';
-import 'monaco-editor/esm/vs/editor/standalone/browser/quickAccess/standaloneGotoSymbolQuickAccess.js';
-import 'monaco-editor/esm/vs/editor/standalone/browser/quickAccess/standaloneCommandsQuickAccess.js';
-import 'monaco-editor/esm/vs/editor/standalone/browser/quickInput/standaloneQuickInputService.js';
-import 'monaco-editor/esm/vs/editor/standalone/browser/referenceSearch/standaloneReferenceSearch.js';
-import 'monaco-editor/esm/vs/editor/standalone/browser/toggleHighContrast/toggleHighContrast.js';
+import "monaco-editor/esm/vs/editor/standalone/browser/accessibilityHelp/accessibilityHelp.js";
+import "monaco-editor/esm/vs/editor/standalone/browser/inspectTokens/inspectTokens.js";
+import "monaco-editor/esm/vs/editor/standalone/browser/iPadShowKeyboard/iPadShowKeyboard.js";
+import "monaco-editor/esm/vs/editor/standalone/browser/quickAccess/standaloneHelpQuickAccess.js";
+import "monaco-editor/esm/vs/editor/standalone/browser/quickAccess/standaloneGotoLineQuickAccess.js";
+import "monaco-editor/esm/vs/editor/standalone/browser/quickAccess/standaloneGotoSymbolQuickAccess.js";
+import "monaco-editor/esm/vs/editor/standalone/browser/quickAccess/standaloneCommandsQuickAccess.js";
+import "monaco-editor/esm/vs/editor/standalone/browser/quickInput/standaloneQuickInputService.js";
+import "monaco-editor/esm/vs/editor/standalone/browser/referenceSearch/standaloneReferenceSearch.js";
+import "monaco-editor/esm/vs/editor/standalone/browser/toggleHighContrast/toggleHighContrast.js";
 
-import * as monaco from 'monaco-editor/esm/vs/editor/editor.api.js';
-import * as vscode from 'vscode';
+import * as monaco from "monaco-editor/esm/vs/editor/editor.api.js";
+import * as vscode from "vscode";
 
-import * as JsonFacade from '@diesel-parser/json-schema-facade-ts';
-import {DieselMonaco} from '@diesel-parser/monaco';
-import {buildWorkerDefinition} from 'monaco-editor-workers';
-import {DieselParserFacade} from '@diesel-parser/ts-facade';
+import * as JsonFacade from "@diesel-parser/json-schema-facade-ts";
+import { DieselMonaco } from "@diesel-parser/monaco";
+import { buildWorkerDefinition } from "monaco-editor-workers";
+import { DieselParserFacade } from "@diesel-parser/ts-facade";
+import { initialSchema, initialValue } from "./initdata";
 
-buildWorkerDefinition('.', new URL('', window.location.href).href, false);
+buildWorkerDefinition(".", new URL("", window.location.href).href, false);
 
-const LANGUAGE_ID1 = 'json1';
-const LANGUAGE_ID2 = 'json2';
-const MODEL_URI1 = 'inmemory://editor1.json1';
+const LANGUAGE_ID1 = "json1";
+const LANGUAGE_ID2 = "json2";
+const MODEL_URI1 = "inmemory://editor1.json1";
 const MONACO_URI1 = monaco.Uri.parse(MODEL_URI1);
-const MODEL_URI2 = 'inmemory://editor2.json2';
+const MODEL_URI2 = "inmemory://editor2.json2";
 const MONACO_URI2 = monaco.Uri.parse(MODEL_URI2);
 
 monaco.languages.register({
-    id: LANGUAGE_ID1,
-    extensions: ['.json1'],
-    aliases: ['JSON1', 'json1'],
-    mimetypes: ['application/json']
+  id: LANGUAGE_ID1,
+  extensions: [".json1"],
+  aliases: ["JSON1", "json1"],
+  mimetypes: ["application/json"],
 });
 
 monaco.languages.register({
-    id: LANGUAGE_ID2,
-    extensions: ['.json2'],
-    aliases: ['JSON2', 'json2'],
-    mimetypes: ['application/json']
+  id: LANGUAGE_ID2,
+  extensions: [".json2"],
+  aliases: ["JSON2", "json2"],
+  mimetypes: ["application/json"],
 });
 
-const model1 = monaco.editor.createModel('{}', LANGUAGE_ID1, MONACO_URI1);
-const model2 = monaco.editor.createModel('{}', LANGUAGE_ID2, MONACO_URI2);
+const model1 = monaco.editor.createModel(
+  JSON.stringify(initialSchema, undefined, "  "),
+  LANGUAGE_ID1,
+  MONACO_URI1
+);
+const model2 = monaco.editor.createModel(
+  JSON.stringify(initialValue, undefined, "  "),
+  LANGUAGE_ID2,
+  MONACO_URI2
+);
 
 const options = {
-    glyphMargin: false,
-    lightbulb: {
-        enabled: true
-    },
-    automaticLayout: false,
-    'semanticHighlighting.enabled': true,
-    minimap: {
-        enabled: false
-    }
+  glyphMargin: false,
+  lightbulb: {
+    enabled: true,
+  },
+  automaticLayout: false,
+  "semanticHighlighting.enabled": true,
+  minimap: {
+    enabled: false,
+  },
 };
 
-export const editor1 = monaco.editor.create(document.getElementById('editor1')!, {
+export const editor1 = monaco.editor.create(
+  document.getElementById("editor1")!,
+  {
     ...options,
-    model: model1
-});
+    model: model1,
+  }
+);
 
-export const editor2 = monaco.editor.create(document.getElementById('editor2')!, {
+export const editor2 = monaco.editor.create(
+  document.getElementById("editor2")!,
+  {
     ...options,
-    model: model2
-});
+    model: model2,
+  }
+);
 
 // @ts-ignore
-window['editor1'] = editor1;
+window["editor1"] = editor1;
 // @ts-ignore
-window['editor2'] = editor2;
+window["editor2"] = editor2;
 
 const docs = vscode.workspace.textDocuments;
 const vscodeDocument1 = docs[0];
 const vscodeDocument2 = docs[1];
 
-const INITIAL_VALUE = {}; 
+const INITIAL_VALUE = {};
 
 const parser1: DieselParserFacade = JsonFacade.getJsonParser(INITIAL_VALUE);
-let parser2 : DieselParserFacade = JsonFacade.getJsonParser(INITIAL_VALUE);
+let parser2: DieselParserFacade = JsonFacade.getJsonParser(INITIAL_VALUE);
 
-model1.onDidChangeContent(e => {
-    try {
-        const newSchema = JSON.parse(model1.getValue());
-        parser2 = JsonFacade.getJsonParser(newSchema);
-    } catch (e) {
-        parser2 = JsonFacade.getJsonParser(INITIAL_VALUE);
-    }
+model1.onDidChangeContent((e) => {
+  try {
+    const newSchema = JSON.parse(model1.getValue());
+    parser2 = JsonFacade.getJsonParser(newSchema);
+  } catch (e) {
+    parser2 = JsonFacade.getJsonParser(INITIAL_VALUE);
+  }
 });
 
 function getTokenType(styleName: string): string | undefined {
-    switch (styleName) {
-        case "number":
-        case "string":
-        case "keyword":
-            return styleName;
-        case "attr":
-            return "property";
-    }
-    return undefined;
+  switch (styleName) {
+    case "number":
+    case "string":
+    case "keyword":
+      return styleName;
+    case "attr":
+      return "property";
+  }
+  return undefined;
 }
 
-const TOKEN_TYPES = ['number', 'string', 'keyword', 'property'];
+const TOKEN_TYPES = ["number", "string", "keyword", "property"];
 
 const dieselMonaco1 = new DieselMonaco(
-    MODEL_URI1,
-    MONACO_URI1,
-    LANGUAGE_ID1,
-    () => parser1,
-    undefined,
-    TOKEN_TYPES,
-    getTokenType,
-    vscodeDocument1
+  MODEL_URI1,
+  MONACO_URI1,
+  LANGUAGE_ID1,
+  () => parser1,
+  undefined,
+  TOKEN_TYPES,
+  getTokenType,
+  vscodeDocument1
 );
 
 const dieselMonaco2 = new DieselMonaco(
-    MODEL_URI2,
-    MONACO_URI2,
-    LANGUAGE_ID2,
-    () => parser2,
-    undefined,
-    TOKEN_TYPES,
-    getTokenType,
-    vscodeDocument2
+  MODEL_URI2,
+  MONACO_URI2,
+  LANGUAGE_ID2,
+  () => parser2,
+  undefined,
+  TOKEN_TYPES,
+  getTokenType,
+  vscodeDocument2
 );
 
 dieselMonaco1.registerCompletion();
 dieselMonaco1.registerSemanticHighlight();
 model1.onDidChangeContent((_event) => {
-    dieselMonaco1.validateDocument();
+  dieselMonaco1.validateDocument();
 });
 dieselMonaco1.validateDocument();
 
 dieselMonaco2.registerCompletion();
 dieselMonaco2.registerSemanticHighlight();
 model2.onDidChangeContent((_event) => {
-    dieselMonaco2.validateDocument();
+  dieselMonaco2.validateDocument();
 });
 dieselMonaco2.validateDocument();
-
