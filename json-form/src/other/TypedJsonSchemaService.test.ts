f47bedb08759d83c4bc83e11dead549f7e8ff88e
Frank Wagner <agilecoderfrank@gmail.com>
2026-04-17 15:25:36 +0200

    connect validate and propose

--- json-form/src/other/TypedJsonSchemaService.test.ts
+++ json-form/src/other/TypedJsonSchemaService.test.ts
@@ -1,6 +1,8 @@
 import { describe, expect, test } from 'vitest';
 import { TypedJsonSchemaService } from './TypedJsonSchemaService';
 import { readFileSync } from 'fs';
+import { parseJsonValue, stringify } from '../JsonValue';
+import { JsPath } from '../JsPath';
 
 describe('TypedJsonSchemaService', async () => {
   const service = await TypedJsonSchemaService.load(
@@ -15,4 +17,37 @@ describe('TypedJsonSchemaService', async () => {
     const version = service.typedJson.version();
     expect(version).toEqual('0.9.1');
   });
+
+  test('validate', async () => {
+    const errors = await service
+      .validate(
+        parseJsonValueUnsafe(`{"type":"number"}`),
+        parseJsonValueUnsafe(`"hello"`),
+      )
+      .then((r) => r.getErrors());
+    expect(errors).toEqual([{ message: 'expected type: number', path: '' }]);
+  });
+
+  test('propose', async () => {
+    const values = await service
+      .propose(
+        parseJsonValueUnsafe(`{"type":"number"}`),
+        parseJsonValueUnsafe(`""`),
+        JsPath.empty,
+      )
+      .then((r) =>
+        r.flatMap((v) =>
+          stringify(v)
+            .map((v) => [v])
+            .withDefaultSupply(() => []),
+        ),
+      );
+    expect(values).toEqual(['0']);
+  });
 });
+
+function parseJsonValueUnsafe(json: string) {
+  return parseJsonValue(json).withDefaultSupply(() => {
+    throw new Error('Invalid JSON');
+  });
+}
--- json-form/src/other/TypedJsonSchemaService.ts
+++ json-form/src/other/TypedJsonSchemaService.ts
@@ -1,7 +1,18 @@
-import { TypedJson } from 'typed-json-ts';
-import { JsonValue } from '../JsonValue';
+import {
+  BasicOutput,
+  SuggestionOutput,
+  toValueWithMeta,
+  TypedJson,
+} from 'typed-json-ts';
+import { JsonValue, parseJsonValue, stringify } from '../JsonValue';
 import { JsPath } from '../JsPath';
-import { SchemaService, ValidationResult } from '../SchemaService';
+import {
+  SchemaRenderer,
+  SchemaService,
+  ValidationError,
+  ValidationResult,
+} from '../SchemaService';
+import { map2 } from 'tea-cup-fp';
 
 export class TypedJsonSchemaService implements SchemaService {
   static async load(
@@ -15,13 +26,69 @@ export class TypedJsonSchemaService implements SchemaService {
   private constructor(public readonly typedJson: TypedJson) {}
 
   validate(schema: JsonValue, instance: JsonValue): Promise<ValidationResult> {
-    throw new Error('Method not implemented.');
+    return map2(
+      stringify(schema),
+      stringify(instance),
+      (schemaValue: string, instanceValue) =>
+        this.typedJson.validate(schemaValue, instanceValue),
+    )
+      .map((r) => r.then((r) => new TypedJsonValidationResult(r)))
+      .withDefaultSupply(() => Promise.reject('Broken schema or instance'));
   }
+
   propose(
     schema: JsonValue,
     instance: JsonValue,
     path: JsPath,
   ): Promise<readonly JsonValue[]> {
+    return map2(
+      stringify(schema),
+      stringify(instance),
+      async (schemaValue: string, instanceValue) => {
+        const suggestOutputs = await this.typedJson.suggest(
+          schemaValue,
+          instanceValue,
+          path.format(),
+          true,
+        );
+        const vs = toValueWithMeta(suggestOutputs)
+          .map((v) => v.value)
+          .map((v) => '' + v);
+        return vs.map((v) =>
+          parseJsonValue(v).withDefaultSupply(() => {
+            throw new Error('Invalid JSON');
+          }),
+        );
+      },
+    ).withDefaultSupply(() => Promise.reject('Broken schema or instance'));
+  }
+}
+
+// function flattenSuggestionOutputValues(
+//   suggestOutput: readonly SuggestionOutput[],
+// ): readonly string[] {
+//   return [];
+// }
+
+class TypedJsonValidationResult implements ValidationResult {
+  constructor(private readonly r: BasicOutput) {}
+
+  getErrors(): readonly ValidationError[] {
+    return this.r.valid
+      ? []
+      : (this.r.errors?.map((e) => ({
+          message: e.error,
+          path: e.instanceLocation.slice(1),
+        })) ?? []);
+  }
+
+  getRenderers(): ReadonlyMap<string, SchemaRenderer> {
+    throw new Error('Method not implemented.');
+  }
+  getFormats(path: JsPath): readonly string[] {
+    throw new Error('Method not implemented.');
+  }
+  getDiscriminator(path: JsPath): string | undefined {
     throw new Error('Method not implemented.');
   }
 }
