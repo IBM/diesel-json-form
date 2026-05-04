eae9e22fcb6e507572e0f1c97bbbb54237703bf9
Frank Wagner <agilecoderfrank@gmail.com>
2026-05-04 10:56:35 +0200

    use typed-json-ts 0.11.4

--- json-form/src/other/TypedJsonSchemaService.test.ts
+++ json-form/src/other/TypedJsonSchemaService.test.ts
@@ -17,7 +17,7 @@ describe('TypedJsonSchemaService', async () => {
 
   test('version', async () => {
     const version = service.typedJson.version();
-    expect(version).toEqual('0.11.3');
+    expect(version).toEqual('0.11.4');
   });
 
   test('validate', async () => {
