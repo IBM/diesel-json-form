4216ca2ab6793d88284e5d7143543674c2daddcb
Remi Van Keisbelck <remi@rvkb.com>
2026-06-22 10:31:08 +0200

    also build webpack

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 npm install && \
 (cd json-form && npm run build) && \
-(cd sandbox && npm run build)
+(cd sandbox && npm run build) && \
+(cd sandbox-webpack && npm run build) 
