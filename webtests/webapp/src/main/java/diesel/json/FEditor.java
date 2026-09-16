a55fff88cbd5984a07cc9b713833cb5cfb36c735
Remi Van Keisbelck <remi@rvkb.com>
2026-09-16 09:48:19 +0200

    webtests

--- webtests/webapp/src/main/java/diesel/json/FEditor.java
+++ webtests/webapp/src/main/java/diesel/json/FEditor.java
@@ -2,53 +2,44 @@ package diesel.json;
 
 import com.pojosontheweb.selenium.AbstractPageObject;
 import com.pojosontheweb.selenium.Findr;
-import org.openqa.selenium.JavascriptExecutor;
 
-import java.util.Arrays;
+import static com.pojosontheweb.selenium.Findrs.attrEquals;
 
 public class FEditor extends AbstractPageObject {
 
     private final String id;
-    private final JavascriptExecutor js = (JavascriptExecutor) getDriver();
-    private final String windowEditorRef;
 
     public FEditor(Findr f, String id) {
         super(f.$("#" + id));
         this.id = id;
-        this.windowEditorRef = "window." + this.id;
     }
 
     public FEditor clearText() {
-        getFindr().eval(e -> {
-            js.executeScript(windowEditorRef + ".setValue('');");
-            return true;
-        });
+        getFindr().clear();
         return this;
     }
 
     public FEditor typeText(String text) {
-        getFindr().eval(e -> {
-            String[] escaped = text.split("\n");
-            Arrays.asList(escaped).forEach(line -> {
-                String script = windowEditorRef +
-                        ".setValue(" +
-                        windowEditorRef +
-                        ".getValue() + '" +
-                        line + "\\n" +
-                        "');";
-                js.executeScript(script);
-            });
+        getFindr().sendKeys(text);
+        // getFindr().eval(e -> {
+        //     String[] escaped = text.split("\n");
+        //     Arrays.asList(escaped).forEach(line -> {
+        //         String script = windowEditorRef +
+        //                 ".setValue(" +
+        //                 windowEditorRef +
+        //                 ".getValue() + '" +
+        //                 line + "\\n" +
+        //                 "');";
+        //         js.executeScript(script);
+        //     });
 
-            return true;
-        });
+        //     return true;
+        // });
         return this;
     }
 
     public FEditor assertText(String expected) {
-        getFindr().where(e -> {
-            String value = (String) js.executeScript("return " + windowEditorRef + ".getValue();");
-            return expected.equals(value.trim());
-        }).eval();
+        getFindr().where(attrEquals("value", expected)).eval();
         return this;
     }
 
@@ -58,7 +49,7 @@ public class FEditor extends AbstractPageObject {
     }
 
     public FEditor focus() {
-        getFindr().$(".view-lines").click();
+        getFindr().click();
         return this;
     }
 }
